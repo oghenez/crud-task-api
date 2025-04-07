d6a1783ecd2eedbcdf0d3af23696a2fcbd9a199f
USERPC\User <3947396+oghenez@users.noreply.github.com>
2025-04-07 08:25:07 +0100

    added comment o the code

--- database/mongoose.js
+++ database/mongoose.js
@@ -5,6 +5,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/taskmanager')
     console.log('DB Connected!')
 })
   .catch((error) => {
-    console.log(error)
+    console.log(`Error connection to DB: ${error}`)
 });
 module.exports = mongoose;
